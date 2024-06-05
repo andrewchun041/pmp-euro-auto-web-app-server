6181b580d3e08f3ba16aaa67baf9795e0850d9ec
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-06-05 14:57:02 -0700

    add price to car_parts data and migration

--- migrations/20240604234920_create_car_parts_table.js
+++ migrations/20240604234920_create_car_parts_table.js
@@ -6,9 +6,10 @@ exports.up = function(knex) {
     return knex.schema.createTable('car_parts', function(table) {
         table.increments('id').primary(); // Primary key that auto increments
         table.integer('part_stock').notNullable().unique(); // Stock number, starting at 100001
+        table.integer('car_id').unsigned().notNullable(); // Foreign key referencing 'cars' table
         table.string('part_name').notNullable(); // Part name
         table.integer('number_of_pieces').notNullable(); // Number of pieces, int
-        table.integer('car_id').unsigned().notNullable(); // Foreign key referencing 'cars' table
+        table.decimal('price', 10, 2).notNullable();  // Add price column
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
     
